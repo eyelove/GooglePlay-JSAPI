5d7b312e4ba6b2fbb7b56d8035b02671a654a4c0
KimDeokJung <djkim@onnuridmc.com>
2017-08-23 20:56:51 +0900

    edit ios genres return values

--- scrapers/app.js
+++ scrapers/app.js
@@ -43,7 +43,7 @@ module.exports = {
 				author_url: r.sellerUrl,
 				rating: r.averageUserRating,
 				ratingcount: r.userRatingCount,
-				genre: genres.join(',')
+				genre: genres.join('/')
 			}
 		} else {
 			return {
