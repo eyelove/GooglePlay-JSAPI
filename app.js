0c3b504547e624da5fb44ac12e9471074274a24f
wowowwww <38094756+theglory@users.noreply.github.com>
2021-03-19 17:30:36 +0900

    app js
    ios trackid 추가

---
+++ app.js
@@ -0,0 +1,56 @@
+module.exports = {
+	/**
+	* Parses the given html to scrap the desired metadata
+	*
+	* @reference Same API Syntax as iTunes http://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html#searchexamples
+	*
+	* @param String data
+	* @return Object
+		@prop String title
+		@prop String description
+		@prop String storeurl
+		@prop String version
+		@prop String iconurl
+		@prop String bundle
+		@prop String price
+		@prop String rating
+		@prop String rationcount
+		@prop String genre
+    @prop String author
+    @prop String author_url
+	*/
+	parse: function(data) {
+
+		var res = JSON.parse(data);
+
+		if (res.results.length>0) {
+			var r = res.results[0];
+			
+			var genres = r.genres;
+			genres.forEach(function(value, index){
+				genres[index] = value.trim();
+			});
+
+			return {
+				title: r.trackName,
+				storeurl: r.trackViewUrl,
+				description: r.description,
+				version: r.version,
+				iconurl: r.artworkUrl512,
+				bundle: r.bundleId,
+				trackid: r.trackId,
+				price: r.price,
+				author: r.sellerName,
+				author_url: r.sellerUrl,
+				rating: r.averageUserRating,
+				ratingcount: r.userRatingCount,
+				genre: genres.join('/'),
+				screen_list: r.screenshotUrls
+			}
+		} else {
+			return {
+				"error": 404
+			};
+		}
+	}
+}
